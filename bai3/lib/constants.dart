8e0885d5adc2d7bf753d9e45740a9ec25db7a51a
nguyennamthien <nguyennamthien00@gmail.com>
2023-08-19 00:03:33 +0700

    first commit

---
+++ bai3/lib/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+// ignore: constant_identifier_names
+const String google_api_key = "API_KEY";
+const Color primaryColor = Color(0xFF7B61FF);
+const double defaultPadding = 16.0;
